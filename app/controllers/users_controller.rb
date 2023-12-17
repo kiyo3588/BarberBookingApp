8b4e4bfa2de1b59e00b0c8bc06716aea5c0dfa41
kiyo3588 <kiyoohara0627@email.com>
2023-12-17 11:17:50 +0900

    5. 5. 2 まずは登録の失敗に対応しよう

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,15 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # 保存に成功した場合は、ここに記述した処理が実行されます。
+    else
+      render :new
+    end
   end
 end

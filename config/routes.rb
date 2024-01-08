57eb6da7084c6add7af5ef43a78ab945ec3c4ba8
kiyo3588 <kiyoohara0627@email.com>
2024-01-09 08:47:46 +0900

    料金表ページ・リンク作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,24 +8,6 @@ Rails.application.routes.draw do
 
   resources :users
 
-  # メニュー項目の一覧表
-  get '/menu_items', to: 'menu_items#index'
-
-  # メニュー項目の詳細表示
-  get '/menu_items/:id', to: 'menu_items#show', as: 'menu_item'
-
-  # 新しいメニュー項目の作成フォーム
-  get '/menu_items/new', to: 'menu_items#new', as: 'new_menu_item'
-
-  # メニュー項目の作成
-  post '/menu_items', to: 'menu_items#create'
-
-  # メニュー項目の編集フォーム
-  get '/menu_items/:id/edit', to: 'menu_items#edit', as: 'edit_menu_item'
-
-  # メニュー項目の更新
-  patch '/menu_items/:id', to: 'menu_items#update'
-
-  # メニュー項目の削除
-  delete '/menu_items/:id', to: 'menu_items#destroy'
+  # メニュー料金一覧へのルーティングを追加
+  resources :menu_items, only: [:index]
 end

03fe048e91ca8026827c12c69b8302b8c3c1e2a4
kiyo3588 <kiyoohara0627@email.com>
2024-01-12 08:59:31 +0900

    menu-model バリデーション

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,2 +1,5 @@
 class MenuItem < ApplicationRecord
+  validates :menu_name, presence: true, uniqueness: true
+  validates :description, length: { maximum: 100 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end

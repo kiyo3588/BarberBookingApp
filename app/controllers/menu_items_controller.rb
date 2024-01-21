84e3f88257921a9fc32e9496badd8e5d37997f16
kiyo3588 <kiyoohara0627@email.com>
2024-01-21 22:00:41 +0900

    メニューでセットアクッションの設定（before_action)

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,4 +1,5 @@
 class MenuItemsController < ApplicationController
+  before_action :set_menu_item, only: [:edit, :update]
 
   def index
     @menu_items = MenuItem.order(:order)
@@ -19,19 +20,22 @@ class MenuItemsController < ApplicationController
   end
 
   def edit
-    @menu_item = MenuItem.find(params[:id])
   end
 
   def update
-    @menu_item = MenuItem.find(params[:id])
     if @menu_item.update(menu_item_params)
-      redirect_to menu_items_path, notice: 'メニューアイテムが更新されました。'
+      flash[:success] = "メニューアイテムが更新されました。"
+      redirect_to menu_items_path
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
     private
+
+    def set_menu_item
+      @menu_item = MenuItem.find(params[:id])
+    end
 
     def menu_item_params
       params.require(:menu_item).permit(:menu_name, :description, :price, :order)

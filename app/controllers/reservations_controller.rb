70716b28872ca0f6782f54b7b0873abe196ae9b4
kiyo3588 <kiyoohara0627@email.com>
2024-04-08 08:56:33 +0900

    予約画面の簡略表示

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,7 +4,7 @@ class ReservationsController < ApplicationController
     # 週単位のカレンダーを表示するための追加ロジックが必要な場合はここに記述
     @start_date = params[:start_date].to_date
     end_date = @start_date.end_of_week
-  
+    @date_range = (@start_date..end_date).to_a
     @reservations = Reservation.where(day: @start_date..end_date)
   end
 end

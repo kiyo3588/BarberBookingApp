ca22c3400d38341d91e16f5e574641db07adf268
kiyo3588 <kiyoohara0627@email.com>
2024-04-09 01:11:23 +0900

    選択日の前後3日間表示

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,8 +3,10 @@ class ReservationsController < ApplicationController
   def show_week
     # 週単位のカレンダーを表示するための追加ロジックが必要な場合はここに記述
     @start_date = params[:start_date].to_date
-    end_date = @start_date.end_of_week
-    @date_range = (@start_date..end_date).to_a
+    end_date = @start_date + 3.days
+    start_date = @start_date - 3.days
+
+    @date_range = (start_date..end_date).to_a
     @reservations = Reservation.where(day: @start_date..end_date)
   end
 end
